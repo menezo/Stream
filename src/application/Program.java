6dc6048b193b27544acaa71c8a558980416be9fb
Joao Menezes <joaovitormenezesm12@gmail.com>
2022-10-13 15:32:20 -0300

    Creating Stream

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,18 @@
 package application;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);
+		
+		Stream<Integer> st1 = list.stream();
+		
+		System.out.println(Arrays.toString(st1.toArray()));
 
 	}
 
